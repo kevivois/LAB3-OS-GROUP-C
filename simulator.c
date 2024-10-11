beca70909f3ce86e36fa699c846724e1069da82d
anouillz <anagoncalves7@icloud.com>
2024-10-11 18:36:29 +0200

    Methods FCFS, pr, and strf and optimized the program

--- simulator.c
+++ simulator.c
@@ -1,257 +1,321 @@
-#include <complex.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdlib.h>  // For atoi
+
 #define RR_QUANTUM 2
 #define CNTXT_SWITCH 1
 
-//Process IN class (id, arrival time, execution time, priority)
-typedef struct ProcessIn {
-    int id;
+typedef struct processStruct {
+    int pid;
     int arrival_time;
     int execution_time;
     int priority;
-} Process_in;
+} process_t;
 
-//Process OUT class (id,turnaround time =completion-arrival , waiting time = turnaround-execution, preemptions)
-typedef struct ProcessOut {
-    int id;
+typedef struct processReturn {
+    int pid;
     int turnaround_time;
     int waiting_time;
-    int nb_preemptions;
-} Process_out;
+    int prempted_n;
+} processReturn_t;
 
-//Node IN class
-typedef struct NodeStructIn {
-    Process_in process;
-    struct NodeStructIn * next;
-} Node_in;
-
-//Node Out class
-typedef struct NodeStructOut {
-    Process_out process;
-    struct NodeStructOut * next;
-} Node_out;
-
-//IN : When creating data is used to add a node to the end of a linked list
-void add_processIn(Node_in **head, Process_in new_process) {
-    Node_in *new_node = (Node_in *)malloc(sizeof(Node_in));
-    new_node->process = new_process;
-    new_node->next = NULL;
-
-    if (*head == NULL) {
-        *head = new_node;
-    } else {
-        Node_in * current = *head;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = new_node;
-    }
-}
-
-//OUT : When processing data is used to add a node to the end of a linked list
-void add_processOut(Node_out **head, Process_out new_processOut) {
-    Node_out *new_node = (Node_out *)malloc(sizeof(Node_out)); // Allocate memory for a new node
-    new_node->process = new_processOut;                        // Set the process_out data
-    new_node->next = NULL;                                     // This will be the last node
-
-    if (*head == NULL) {                                       // If the list is empty
-        *head = new_node;                                      // New node becomes the head
-    } else {
-        Node_out *current = *head;
-        while (current->next != NULL) {                        // Traverse to the last node
-            current = current->next;
-        }
-        current->next = new_node;                              // Link the last node to the new node
-    }
-}
-
-//Print all the process of a linked list IN
-void print_processes_in(Node_in *head) {
-    Node_in *current = head;
-    while (current != NULL) {
-        printf("Process ID: %d, Arrival Time: %d, Execution Time: %d, Priority: %d\n",
-               current->process.id,
-               current->process.arrival_time,
-               current->process.execution_time,
-               current->process.priority);
-        current = current->next;  // Move to the next node
-    }
-    printf("\n");
-}
-//Print all the process of a linked list OUT
-void print_processes_out(Node_out *head) {
-    Node_out *current = head;
-    while (current != NULL) {
-        printf("Process ID: %d, turnaround_time: %d, waiting_time: %d, nb_preemptions: %d, \n",
-               current->process.id,
-               current->process.turnaround_time,
-               current->process.waiting_time,
-               current->process.nb_preemptions);
-        current = current->next;  // Move to the next node
-    }
-    printf("\n");
-}
-
-//Avoid memory links by removing the head in the memory
-void free_process_list(Node_in *head) {
-    Node_in *current = head;
-    Node_in *next;
-    while (current != NULL) {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-}
-
-//Create all the data (adding in list process) by reading tasks.csv
-void process_file(Node_in **process_list) {
-    char filepath[] = "./tasks.csv";
-    FILE *file = fopen(filepath, "r");
+process_t* readFile(const char* filename, int* count) {
+    FILE* file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
-        return;
+        exit(EXIT_FAILURE);
     }
+
     char line[256];
-    //Go through all the lines and parse them
+    *count = 0;
     while (fgets(line, sizeof(line), file)) {
-        char *col1, *col2, *col3, *col4;
-        col1 = strtok(line, " ");
-        col2 = strtok(NULL, " ");
-        col3 = strtok(NULL, " ");
-        col4 = strtok(NULL, " ");
-        //Create process and add it to the linked list
-        Process_in process = {atoi(col1), atoi(col2), atoi(col3), atoi(col4)};
-        add_processIn(process_list, process);
+        (*count)++;
     }
-    print_processes_in(*process_list);
+
+    process_t* processes = (process_t*)malloc((*count) * sizeof(process_t));
+    if (processes == NULL) {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
+    }
+
+    rewind(file);
+
+    for (int i = 0; i < *count; i++) {
+        fscanf(file, "%d %d %d %d", &processes[i].pid, &processes[i].arrival_time,
+               &processes[i].execution_time, &processes[i].priority);
+    }
+
+    fclose(file);
+    return processes;
+}
+
+void writeFileExecution(const char* filename, processReturn_t* processes, int n) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(file, "pid,turnaround_time,waiting_time,prempted_n\n");
+
+    for (int i = 0; i < n; i++) {
+        fprintf(file, "%d,%d,%d,%d\n", processes[i].pid, processes[i].turnaround_time, processes[i].waiting_time, processes[i].prempted_n);
+    }
+
     fclose(file);
 }
 
-//Write the output file (process id, turnaround time, waiting time, num preempted)
-void write_file(Node_out * e){
-    FILE *myStream = fopen("execution.csv", "w");
-    print_processes_out(e);
-    fclose(myStream);
-}
-
-//FCFS : executed in order of arrival time
-Node_out fcfs(Node_in *head) {
-    Node_out *head_of_fcfs_list = NULL;
-    Node_in *current = head;
-
-    int timer = 0;
-    int completionTime = 0;
-    int turnaroundTime;
-    int waitingTime;
-
-    while (current != NULL) {
-        // goes to arrival time -> speed
-        if (timer < current->process.arrival_time) {
-            timer = current->process.arrival_time;
-        }
-
-        // execution of the current process
-        // calculate values needed
-        timer += current->process.execution_time;
-        completionTime = timer;
-        turnaroundTime = completionTime - current->process.arrival_time;
-        waitingTime = turnaroundTime - current->process.execution_time;
-
-        // create new processOut and add it to the list
-        Process_out processOut = {current->process.id, turnaroundTime, waitingTime, 0};
-        add_processOut(&head_of_fcfs_list, processOut);
-
-        current = current->next;
+processReturn_t* FCFS(process_t* processes, int n){
+    processReturn_t* returnValues = (processReturn_t*)malloc(n * sizeof(processReturn_t));
+    if (returnValues == NULL) {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
     }
 
-    return *head_of_fcfs_list;
+    int time = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (time < processes[i].arrival_time) {
+            time = processes[i].arrival_time;
+        }
+        returnValues[i].pid = processes[i].pid;
+        if (time > processes[i].arrival_time) {
+            returnValues[i].waiting_time = time - processes[i].arrival_time;
+        } else {
+            returnValues[i].waiting_time = 0;
+        }
+        returnValues[i].turnaround_time = processes[i].execution_time + returnValues[i].waiting_time;
+        returnValues[i].prempted_n = 0;
+
+        time += processes[i].execution_time;
+    }
+    return returnValues;
 }
 
-Node_out rr(Node_in *head) {
-    Node_out *head_of_rr_list = NULL;
-    Node_in *current = head;
+processReturn_t* RR(process_t* processes, int n){
+    processReturn_t* returnValues = (processReturn_t*) malloc(n * sizeof(processReturn_t));
+    if (returnValues == NULL) {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
+    }
 
-    int timer = 0;
-    int nbPreemptions = 0;
+    int remaining_time[n];
+    int completed = 0;
+    int current_time = 0;
+    int prempted_count[n];
+    int waiting_time[n];
+    int turnaround_time[n];
 
-    //create queue to manage process
-    Node_in *queue[100];
-    // index of the first element
-    // when element is deleted from queue -> front++
-    int front = 0;
-    // index where next element will be enqueued
-    // when element is added to the queue -> rear++
-    int rear = 0;
+    while (completed < n) {
+        int done = 1;
+        for (int i = 0; i < n; i++){
+            if (remaining_time[i]>0) {
+                done = 0;
 
-    //add processes to queue at their arrival time and check if queue is empty
-    while (current != NULL || front != rear) {
-        // load processes arriving at the current time
-        while (current != NULL && current->process.arrival_time <= timer) {
-            queue[rear++] = current;
-            current = current->next;
+                if (remaining_time[i] > RR_QUANTUM) {
+                    current_time += RR_QUANTUM;
+                    remaining_time[i] -= RR_QUANTUM;
+                    prempted_count[i]++;
+                } else {
+                    current_time += remaining_time[i];
+                    remaining_time[i] = 0;
+                    completed++;
+
+                    turnaround_time[i] = current_time - processes[i].arrival_time;
+                    waiting_time[i] = turnaround_time[i] - processes[i].execution_time;
+                }
+
+            }
         }
 
-        // If the queue is empty, increment the timer
-        if (front == rear) {
-            timer++;
+        if (done == 1) {
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        returnValues[i].pid = processes[i].pid;
+        returnValues[i].turnaround_time = turnaround_time[i];
+        returnValues[i].waiting_time = waiting_time[i];
+        returnValues[i].prempted_n = prempted_count[i];
+    }
+
+    return returnValues;
+}
+
+processReturn_t* priorityScheduler(process_t* processes, int n) {
+    processReturn_t* returnValues = (processReturn_t*)malloc(n * sizeof(processReturn_t));
+    if (returnValues == NULL) {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
+    }
+
+    int time = 0;
+    int completed = 0;
+    int prempted_count[n];
+    int waiting_time[n];
+    int turnaround_time[n];
+    int remaining_time[n];
+    int priority[n];
+
+    for (int i = 0; i < n; i++) {
+        remaining_time[i] = processes[i].execution_time;
+        priority[i] = processes[i].priority;
+        prempted_count[i] = 0;
+        waiting_time[i] = 0;
+        turnaround_time[i] = 0;
+    }
+
+    int current_process = -1;
+
+    while (completed < n) {
+        int highest_priority = 4;
+        int highest_priority_index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (processes[i].arrival_time <= time && priority[i] < highest_priority && remaining_time[i] > 0) {
+                highest_priority = priority[i];
+                highest_priority_index = i;
+            }
+        }
+
+        if (highest_priority_index == -1) {
+            time++;
             continue;
         }
 
-        // take first process from queue
-        Node_in *proc = queue[front++];
-        int execution_time = proc->process.execution_time;
-
-        // if execution_time <= RR quantum, time_to_run = execution_time, else : RR_Q
-        int time_to_run = execution_time <= RR_QUANTUM ? execution_time : RR_QUANTUM;
-        proc->process.execution_time -= time_to_run;
-        timer += time_to_run;
-
-        // process finishes
-        if (proc->process.execution_time == 0) {
-            int turnaround_time = timer - proc->process.arrival_time;
-            int waiting_time = turnaround_time - execution_time;
-            Process_out processOut = {proc->process.id, turnaround_time, waiting_time, nbPreemptions};
-            add_processOut(&head_of_rr_list, processOut);
-        } else {
-            // if process is not done, put it back in the queue
-            queue[rear++] = proc;
+        if (current_process != -1 && current_process != highest_priority_index && remaining_time[current_process] > 0) {
+            prempted_count[current_process]++;
+            time = time + CNTXT_SWITCH;
         }
 
-        // increment the preemption if there are processes in the queue
-        if (front != rear) {
-            nbPreemptions++;
-            timer += CNTXT_SWITCH; // Adding context switch time
+        current_process = highest_priority_index;
+
+        remaining_time[highest_priority_index]--;
+        time++;
+
+        if (remaining_time[highest_priority_index] == 0) {
+            printf("Process %d completed at time %d\n", processes[highest_priority_index].pid, time);
+            completed++;
+            turnaround_time[highest_priority_index] = time - processes[highest_priority_index].arrival_time;
+            waiting_time[highest_priority_index] = turnaround_time[highest_priority_index] - processes[highest_priority_index].execution_time;
         }
     }
 
-    return *head_of_rr_list;
+    for (int i = 0; i < n; i++) {
+        returnValues[i].pid = processes[i].pid;
+        returnValues[i].turnaround_time = turnaround_time[i];
+        returnValues[i].waiting_time = waiting_time[i];
+        returnValues[i].prempted_n = prempted_count[i];
+    }
 
+    return returnValues;
 }
 
 
+processReturn_t* SRTF(process_t* processes, int n) {
+    processReturn_t* returnValues = (processReturn_t*)malloc(n * sizeof(processReturn_t));
+    if (returnValues == NULL) {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
+    }
 
+    int time = 0;
+    int completed = 0;
+    int prempted_count[n];
+    int waiting_time[n];
+    int turnaround_time[n];
+    int remaining_time[n];
 
+    for (int i = 0; i < n; i++) {
+        remaining_time[i] = processes[i].execution_time;
+        prempted_count[i] = 0;
+        waiting_time[i] = 0;
+        turnaround_time[i] = 0;
+    }
 
-int main() {
-    //INPUT DATA (creation of data)
-    printf("INPUT DATA  \n");
-    Node_in *Input_head = 0;
-    process_file(&Input_head);
-    printf("----------------------------------------------\n");
-    printf("Executing.....  \n");
+    int current_process = -1;
 
-    //Methods
-    //Node_out Output_data = fcfs(Input_head);
-    Node_out Output_data = rr(Input_head);
+    while (completed < n) {
+        int shortest_remaining_time = 1000;
+        int shortest_remaining_time_index = -1;
 
+        for (int i = 0; i < n; i++) {
+            if (processes[i].arrival_time <= time && remaining_time[i] > 0 && remaining_time[i] < shortest_remaining_time) {
+                shortest_remaining_time = remaining_time[i];
+                shortest_remaining_time_index = i;
+            }
+        }
 
+        if (shortest_remaining_time_index == -1) {
+            time++;
+            continue;
+        }
 
-    //OUTPUT DATA (writing data)
-    printf("Output DATA  \n");
-    write_file(&Output_data);
-    free_process_list(Input_head);
-    printf("----------------------------------------------\n");
+        if (current_process != -1 && remaining_time[current_process] > 0 && current_process != shortest_remaining_time_index) {
+            prempted_count[current_process]++;
+            time++;
+        }
+
+        current_process = shortest_remaining_time_index;
+
+        time++;
+        remaining_time[shortest_remaining_time_index]--;
+        shortest_remaining_time--;
+
+        if (remaining_time[shortest_remaining_time_index] == 0) {
+            completed++;
+            turnaround_time[shortest_remaining_time_index] = time - processes[shortest_remaining_time_index].arrival_time;
+            waiting_time[shortest_remaining_time_index] = turnaround_time[shortest_remaining_time_index] - processes[shortest_remaining_time_index].execution_time;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        returnValues[i].pid = processes[i].pid;
+        returnValues[i].turnaround_time = turnaround_time[i];
+        returnValues[i].waiting_time = waiting_time[i];
+        returnValues[i].prempted_n = prempted_count[i];
+    }
+    return returnValues;
+}
+
+int main(int argc, char* argv[]) {
+    int index_of_function = 2;
+
+    char filename[100] = "tasks.csv";
+    process_t* processes;
+    int n = 0;
+    processes = readFile(filename, &n);
+    processReturn_t* returnValues = (processReturn_t*)malloc(n * sizeof(processReturn_t));
+
+    if (returnValues == NULL) {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
+    }
+
+    switch (index_of_function)
+    {
+        case 0:
+            returnValues = FCFS(processes, n);
+            writeFileExecution("output.csv", returnValues, n);
+            break;
+        case 1:
+            returnValues = RR(processes, n);
+            writeFileExecution("output.csv", returnValues, n);
+            break;
+        case 2:
+            returnValues = priorityScheduler(processes, n);
+            writeFileExecution("output.csv", returnValues, n);
+            break;
+        case 3:
+            returnValues = SRTF(processes, n);
+            writeFileExecution("output.csv", returnValues, n);
+            break;
+
+        default:
+            break;
+    }
+
+    free(processes);
+    free(returnValues);
     return 0;
 }
